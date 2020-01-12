9c9b84bc19b1a412d234d6c9d02dcc398b2f28b1
IT小强xqitw.cn <mail@xqitw.cm>
2020-01-12 12:03:14 +0800

    调整

--- src/WinCmd.py
+++ src/WinCmd.py
@@ -25,14 +25,17 @@ class WinCmd:
     FireWallRuleDisplayName = 'WSL 2 Firewall Unlock'
 
     @classmethod
-    def get_wsl2_ip(cls):
+    def get_wsl2_ip(cls, exec_run=True):
         """
         该方法用于获取WSL2的IP
-        @return: str
+        :param exec_run:
+        :return: str
         """
         pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+        cmd = cls.BASH_EXE + ' -c "ifconfig eth0 | grep \'inet \'"'
+        if not exec_run:
+            return cmd
         try:
-            cmd = cls.BASH_EXE + ' -c "ifconfig eth0 | grep \'inet \'"'
             result = cls.read_cmd(cmd)
             wsl_ip = search(pattern, result).group(0)
         except AttributeError:
@@ -40,23 +43,24 @@ class WinCmd:
         return wsl_ip
 
     @classmethod
-    def start_wsl(cls):
+    def start_wsl(cls, exec_run=True):
         """
         启动wsl子系统
         :return:
         """
         cmd = cls.WSCRIPT_EXE
         cmd += ' ' + cls.WSL_BAT_PATH + ' wsl'
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
-    def port_add(cls, wsl_ip, wsl_port, addr='0.0.0.0'):
+    def port_add(cls, wsl_ip, wsl_port, addr='0.0.0.0', exec_run=True):
         """
         添加端口转发
-        @param addr: 监听地址
-        @param wsl_ip: 待转发的IP地址
-        @param wsl_port: 待添加端口号
-        @return:
+        :param addr: 监听地址
+        :param wsl_ip: 待转发的IP地址
+        :param wsl_port: 待添加端口号
+        :param exec_run:
+        :return:
         """
         cmd = cls.POWER_SHELL
         cmd += ' netsh interface portproxy add v4tov4'
@@ -64,50 +68,55 @@ class WinCmd:
         cmd += ' listenaddress=' + addr
         cmd += ' connectport=' + wsl_port
         cmd += ' connectaddress=' + wsl_ip
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
-    def port_del(cls, wsl_port, addr='0.0.0.0'):
+    def port_del(cls, wsl_port, addr='0.0.0.0', exec_run=True):
         """
         删除端口转发
-        @param addr: 监听地址
-        @param wsl_port: 待删除的端口号
-        @return:
+        :param exec_run:
+        :param addr: 监听地址
+        :param wsl_port: 待删除的端口号
+        :return:
         """
         cmd = cls.POWER_SHELL
         cmd += ' netsh interface portproxy delete v4tov4 listenport=' + wsl_port + ' listenaddress=' + addr
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
-    def port_reset(cls):
+    def port_reset(cls, exec_run=True):
         """
         清除所有的端口转发
-        @return:
+        :param exec_run
+        :return:
         """
         cmd = cls.POWER_SHELL
         cmd += ' netsh interface portproxy reset'
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
-    def port_info(cls, is_str=False):
+    def port_info(cls, is_str=False, exec_run=True):
         """
         查看端口转发情况
-        @return:
+        :param exec_run:
+        :param is_str:
+        :return:
         """
         cmd = cls.POWER_SHELL
         cmd += ' netsh interface portproxy show all'
-        info_str = cls.read_cmd(cmd)
+        info_str = cls.read_cmd(cmd, exec_run)
         if is_str:
             return info_str
         return cls.port_info_to_list(info_str)
 
     @classmethod
-    def fire_wall_rule_add(cls, wsl_port, wall_type):
+    def fire_wall_rule_add(cls, wsl_port, wall_type, exec_run=True):
         """
         添加防火墙规则
-        @param wsl_port: str 端口号
-        @param wall_type: str 防火墙类型 Outbound | Inbound
-        @return:
+        :param exec_run:
+        :param wsl_port: str 端口号
+        :param wall_type: str 防火墙类型 Outbound | Inbound
+        :return:
         """
         "New-NetFireWallRule -DisplayName 'test1008611' -Direction 'Inbound' -Action Allow -Protocol TCP"
         wall_name = cls.FireWallRuleDisplayName + wall_type + wsl_port
@@ -115,20 +124,21 @@ class WinCmd:
         cmd += " New-NetFireWallRule -DisplayName '" + wall_name + "'"
         cmd += " -Direction '" + wall_type + "'"
         cmd += " -LocalPort " + wsl_port + " -Action Allow -Protocol TCP"
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
-    def fire_wall_rule_del(cls, wsl_port, wall_type):
+    def fire_wall_rule_del(cls, wsl_port, wall_type, exec_run=True):
         """
         删除防火墙规则
-        @param wsl_port: str 端口号
-        @param wall_type: str 防火墙类型 Outbound | Inbound
-        @return:
+        :param exec_run:
+        :param wsl_port: str 端口号
+        :param wall_type: str 防火墙类型 Outbound | Inbound
+        :return:
         """
         wall_name = cls.FireWallRuleDisplayName + wall_type + wsl_port
         cmd = cls.POWER_SHELL
         cmd += " Remove-NetFireWallRule -DisplayName '" + wall_name + "'"
-        return cls.read_cmd(cmd)
+        return cls.read_cmd(cmd, exec_run)
 
     @classmethod
     def save_bat_script(cls, content):
@@ -144,12 +154,15 @@ class WinCmd:
         return content
 
     @staticmethod
-    def read_cmd(cmd):
+    def read_cmd(cmd, exec_run=True):
         """
         执行命令，并返回输出结果
+        :param exec_run: 
         :param cmd:
         :return:
         """
+        if not exec_run:
+            return cmd
         f = popen(cmd)
         result = f.read().strip()
         f.close()
